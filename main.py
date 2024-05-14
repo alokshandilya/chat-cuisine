9fa1ffb20d699a3d677167e8285a5d320c8d83bb
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-15 04:30:33 +0530

    add html

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Point to the templates folder
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app)
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # Render index.html template
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}

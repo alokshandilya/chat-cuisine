6809cc5a1c5ead4c03f3d9b171eb2a8fc563820e
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-15 07:03:04 +0530

    remove uvicorn

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)

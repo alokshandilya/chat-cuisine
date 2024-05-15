965f20dc5deaac39e9280aa8431b6143f6008ca7
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-15 06:55:35 +0530

    fix localhost

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)

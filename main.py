8c426da4e550ae4b6a3bb41ebf475697e6798b35
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-16 03:03:51 +0530

    remove session_id from track function

--- main.py
+++ main.py
@@ -26,34 +26,35 @@ def handle_order_remove(params: dict, session_id: str):
 @app.post("/webhook")
 async def webhook_handler(request: WebhookRequest):
     query_result = request.queryResult
-    intent_name = query_result['intent']['displayName']
-    session_id = query_result.get('outputContexts', [{}])[0].get('name', '').split('/')[-1]
+    intent_name = query_result["intent"]["displayName"]
+    session_id = (
+        query_result.get("outputContexts",
+                         [{}])[0].get("name", "").split("/")[-1]
+    )
 
-    if intent_name == 'order.add':
-        params = query_result['parameters']
+    if intent_name == "order.add":
+        params = query_result["parameters"]
         return {"fulfillmentText": handle_order_add(params, session_id)}
-    elif intent_name == 'order.remove':
-        params = query_result['parameters']
+    elif intent_name == "order.remove":
+        params = query_result["parameters"]
         return {"fulfillmentText": handle_order_remove(params, session_id)}
-    elif intent_name == 'track.order - context: ongoing-tracking':
-        params = query_result['parameters']
-        return {"fulfillmentText": track_order(params, session_id)}
+    elif intent_name == "track.order - context: ongoing-tracking":
+        params = query_result["parameters"]
+        return {"fulfillmentText": track_order(params)}
 
     else:
         raise HTTPException(status_code=400, detail="Invalid intent")
 
-def track_order(params: dict, session_id: str):
-    order_id = int(params['number'])
+
+def track_order(params: dict):
+    order_id = int(params["number"])
     order_status = db_helper.get_order_status(order_id)
     if order_status:
         fullfillmentText = (
-            f"Your order status for order id: {order_id} is:\n"
-            f"{order_status}"
+            f"Your order status for order id {order_id} is: {order_status}"
         )
     else:
-        fullfillmentText = (
-            f"Sorry, no order found for order id: {order_id}"
-        )
+        fullfillmentText = f"Sorry, no order found for order id: {order_id}"
     return fullfillmentText
 
 

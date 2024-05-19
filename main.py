a9a460e515917343626c8c61301946eb304859de
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-20 04:48:13 +0530

    test deployment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, HTTPException, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from starlette.middleware.sessions import SessionMiddleware
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 import db_helper
@@ -59,46 +58,13 @@ def track_order(params: dict):
     return fullfillmentText
 
 
+# Mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Secret key for session management
-app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
-
-# Mock user database
-fake_users_db = {"testuser": {"username": "testuser", "password": "testpassword"}}
 
 
 @app.get("/", response_class=HTMLResponse)
-async def login_page(request: Request):
-    user = request.session.get("user")
-    if user:
-        return RedirectResponse("/index", status_code=302)
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-@app.post("/", response_class=HTMLResponse)
-async def login(request: Request, username: str = Form(...), password: str = Form(...)):
-    user = fake_users_db.get(username)
-    if user and user["password"] == password:
-        request.session["user"] = user["username"]
-        return RedirectResponse("/index", status_code=302)
-    return templates.TemplateResponse(
-        "login.html", {"request": request, "error": "Invalid credentials"}
-    )
-
-
-@app.get("/logout")
-async def logout(request: Request):
-    request.session.pop("user", None)
-    return RedirectResponse("/", status_code=302)
-
-
-@app.get("/index", response_class=HTMLResponse)
 async def read_root(request: Request):
-    user = request.session.get("user")
-    if not user:
-        return RedirectResponse("/", status_code=302)
-    return templates.TemplateResponse("index.html", {"request": request, "user": user})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 # Serve the Google verification file

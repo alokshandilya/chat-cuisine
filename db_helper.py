2a91d50eac9e19912981410b47a5bdb7c1ae55b7
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-29 12:04:07 +0530

    test deployment using db

--- db_helper.py
+++ db_helper.py
@@ -29,9 +29,21 @@ engine = create_engine(
 )
 
 # Create engine and session local
-engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_order_status(order_id):
+    # Create a session
+    session = Session()
+
+    # Query the database using SQLAlchemy ORM
+    status = session.query(OrderTracking.status).filter_by(order_id=order_id).scalar()
+
+    session.close()
+
+    return status
+
 
 # Association table for many-to-many relationship between orders and food items
 order_items = Table(
@@ -157,48 +169,3 @@ def get_order_status(order_id: int) -> str:
         return tracking.status.value if tracking else None
     finally:
         session.close()
-
-
-# import os
-# from dotenv import load_dotenv
-#
-# from sqlalchemy import create_engine, Column, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-#
-# Base = declarative_base()
-#
-#
-# # Define your OrderTracking table class
-# class OrderTracking(Base):
-#     __tablename__ = 'order_tracking'
-#
-#     order_id = Column(Integer, primary_key=True)
-#     status = Column(String)
-#
-#
-# # Load environment variables from .env file
-# load_dotenv()
-#
-# # Access the password from environment variable
-# db_password = os.getenv("DB_PASSWORD")
-#
-# # Create an engine using pymysql
-# engine = create_engine(
-#     f"mysql+mysqlconnector://avnadmin:{db_password}@mysql-chatcuisine.e.aivencloud.com:17612/chatcuisine")
-#
-#
-# # Create a session maker
-# Session = sessionmaker(bind=engine)
-#
-#
-# def get_order_status(order_id):
-#     # Create a session
-#     session = Session()
-#
-#     # Query the database using SQLAlchemy ORM
-#     status = session.query(OrderTracking.status).filter_by(order_id=order_id).scalar()
-#
-#     session.close()
-#
-#     return status
